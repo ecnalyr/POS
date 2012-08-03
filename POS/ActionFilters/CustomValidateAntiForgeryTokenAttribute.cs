5bed9ac6f9196673bfe80e58223727ba57a8b4bb
Ecnalyr <ecnalyr@gmail.com>
2012-08-03 15:04:51 -0500

    Replaced CustomValidateAntiForgery system with pre-built version

---
+++ POS/ActionFilters/CustomValidateAntiForgeryTokenAttribute.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace POS.ActionFilters
+{
+    using System.Web.Mvc;
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class CustomValidateAntiForgeryTokenAttribute : FilterAttribute, IAuthorizationFilter
+    {
+        /// <summary>
+        /// The ValidateAntiForgeryTokenAttribute.
+        /// </summary>
+        private ValidateAntiForgeryTokenAttribute Validator { get; set; }
+
+        /// <summary>
+        /// The Accept Verbs Attribute.
+        /// </summary>
+        private AcceptVerbsAttribute AcceptVerbsAttribute { get; set; }
+
+        /// <summary>
+        /// Gets the salt.
+        /// </summary>
+        public string Salt { get; private set; }
+
+        /// <summary>
+        /// Gets the verbs.
+        /// </summary>
+        public HttpVerbs Verbs { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomValidateAntiForgeryTokenAttribute"/> class.
+        /// </summary>
+        /// <param name="verbs">The verbs.</param>
+        public CustomValidateAntiForgeryTokenAttribute(HttpVerbs verbs)
+            : this(verbs, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomValidateAntiForgeryTokenAttribute"/> class.
+        /// </summary>
+        /// <param name="verbs">The verbs.</param>
+        /// <param name="salt">The salt.</param>
+        public CustomValidateAntiForgeryTokenAttribute(HttpVerbs verbs, string salt)
+        {
+            Verbs = verbs;
+            Salt = salt;
+
+            AcceptVerbsAttribute = new AcceptVerbsAttribute(Verbs);
+            Validator = new ValidateAntiForgeryTokenAttribute
+            {
+                Salt = Salt
+            };
+        }
+
+        /// <summary>
+        /// Called when authorization is required.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var httpMethodOverride = filterContext.HttpContext.Request.GetHttpMethodOverride();
+
+            var found = false;
+            foreach (var verb in AcceptVerbsAttribute.Verbs)
+            {
+                if (verb.Equals(httpMethodOverride, StringComparison.OrdinalIgnoreCase))
+                {
+                    found = true;
+                }
+            }
+
+            if (found && !filterContext.RequestContext.RouteData.Values["action"].ToString().StartsWith("Json"))
+            {
+                Validator.OnAuthorization(filterContext);
+            }
+        }
+    }
+}
--- POS/Controllers/ControllerBase.cs
+++ POS/Controllers/ControllerBase.cs
@@ -3,10 +3,11 @@ using System.Reflection;
 using System.Web.Mvc;
 using NLog;
 using POS.Domain.Abstract;
+using POS.ActionFilters;
 
 namespace POS.Controllers
 {
-    [UseAntiForgeryTokenOnPostByDefault]
+    [CustomValidateAntiForgeryTokenAttribute(HttpVerbs.Post)]
     public class ControllerBase : Controller
     {
         #region Fields
@@ -29,56 +30,5 @@ namespace POS.Controllers
             }
         }
 
-
-        [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-        public class BypassAntiForgeryTokenAttribute : ActionFilterAttribute
-        {
-        }
-
-        [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-        public class UseAntiForgeryTokenOnPostByDefault : ActionFilterAttribute
-        {
-            public override void OnActionExecuting(ActionExecutingContext filterContext)
-            {
-                if (ShouldValidateAntiForgeryTokenManually(filterContext))
-                {
-                    var authorizationContext = new AuthorizationContext(filterContext.Controller.ControllerContext);
-
-                    //Use the authorization of the anti forgery token, 
-                    //which can't be inhereted from because it is sealed
-                    new ValidateAntiForgeryTokenAttribute().OnAuthorization(authorizationContext);
-                }
-
-                base.OnActionExecuting(filterContext);
-            }
-
-            /// <summary>
-            /// We should validate the anti forgery token manually if the following criteria are met:
-            /// 1. The http method must be POST
-            /// 2. There is not an existing [ValidateAntiForgeryToken] attribute on the action
-            /// 3. There is no [BypassAntiForgeryToken] attribute on the action
-            /// </summary>
-            private static bool ShouldValidateAntiForgeryTokenManually(ActionExecutingContext filterContext)
-            {
-                var httpMethod = filterContext.HttpContext.Request.HttpMethod;
-
-                //1. The http method must be POST
-                if (httpMethod != "POST") return false;
-
-                // 2. There is not an existing anti forgery token attribute on the action
-                var antiForgeryAttributes =
-                    filterContext.ActionDescriptor.GetCustomAttributes(typeof (ValidateAntiForgeryTokenAttribute), false);
-
-                if (antiForgeryAttributes.Length > 0) return false;
-
-                // 3. There is no [BypassAntiForgeryToken] attribute on the action
-                var ignoreAntiForgeryAttributes =
-                    filterContext.ActionDescriptor.GetCustomAttributes(typeof (BypassAntiForgeryTokenAttribute), false);
-
-                if (ignoreAntiForgeryAttributes.Length > 0) return false;
-
-                return true;
-            }
-        }
     }
 }
