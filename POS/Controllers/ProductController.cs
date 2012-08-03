84c4f2ae6068bdb5e056125b655e03abfbd4f251
Ecnalyr <ecnalyr@gmail.com>
2012-08-03 16:06:21 -0500

    Fixed a bug that was created by previous commit

--- POS/Controllers/ProductController.cs
+++ POS/Controllers/ProductController.cs
@@ -61,13 +61,11 @@ namespace POS.Controllers
             return View(_productRepository.Categories);
         }
 
-        [BypassAntiForgeryTokenAttribute]
         public ActionResult ParentCategories()
         {
             return View(_productRepository.ParentCategories);
         }
 
-        [BypassAntiForgeryTokenAttribute]
         public ActionResult ProductList(int category)
         {
             IEnumerable<Product> productList = _productRepository.Products.Where(p => p.CategoryId == category);
