b5a75185b23af4de16ee7465b32615f4dc310bb4
Ecnalyr <ecnalyr@gmail.com>
2012-08-07 13:45:38 -0500

    An entity object cannot be referenced by multiple instances of IEntityChangeTracker

---
+++ POS.Domain/Model/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POS.Domain.Model
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public List<CartLine> CartLines { get; set; }
+
+        public int EstablishmentId { get; set; }
+
+        public virtual Establishment Establishment { get; set; }
+    }
+}
