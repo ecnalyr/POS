9253d40d89d8f02530a1aa6d43a730e5c37404a7
Ecnalyr <ecnalyr@gmail.com>
2012-08-16 10:46:17 -0500

    Began Adding a Generic Repository
    This will be a smoother way of implementing the order processor as well
    as every other repository-related action.

---
+++ POS.Infrastructure/EFRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using POS.Domain.Abstract;
+using System.Data.Objects;
+
+namespace POS.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private EfDbContext _context;
+        public Repository(EfDbContext context)
+        {
+            _context = context;
+        }
+        private IDbSet<T> DbSet
+        {
+            get
+            {
+                return _context.Set<T>();
+            }
+        }
+
+        #region IRepository<T> Members
+
+        public IQueryable<T> Query()
+        {
+            return DbSet.AsQueryable();
+        }
+
+        public void Delete(T entity)
+        {
+            DbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+
+        _context.SaveChanges();
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_context != null)
+                {
+                    _context.Dispose();
+                    _context = null;
+                }
+            }
+        }
+    } 
+}
